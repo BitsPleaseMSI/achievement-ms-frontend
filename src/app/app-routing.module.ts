c29b70dff39435a799fee38bd65869207fbba132
Sreejeet <vsreejeet@gmail.com>
2018-09-29 20:13:22 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component'
+import { AddAchievementComponent } from './add-achievement/add-achievement.component'
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'teacherLogin',
+    component: LoginComponent,
+  },
+  {
+    path: 'myNewAchievement',
+    component: AddAchievementComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
