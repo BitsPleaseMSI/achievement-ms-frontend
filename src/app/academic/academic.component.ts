092a88103a0d941208cddc3ec430cfb346cac910
sreejeet <vsreejeet@gmail.com>
2018-10-20 21:58:19 +0530

    Added academic component

---
+++ src/app/academic/academic.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-academic',
+  templateUrl: './academic.component.html',
+  styleUrls: ['./academic.component.css']
+})
+export class AcademicComponent implements OnInit {
+  achievements$: Object = [];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { DashboardComponent } from './dashboard/dashboard.component'
 import { DetailsComponent } from './details/details.component'
 import { AboutComponent } from './about/about.component'
 import { SettingsComponent } from './settings/settings.component'
+import { AcademicComponent } from './academic/academic.component'
 
 const routes: Routes = [
   {
@@ -47,6 +48,10 @@ const routes: Routes = [
   {
     path: 'dashboard/unapproved',
     component: DashboardComponent,
+  },
+  {
+    path: 'home/academic',
+    component: AcademicComponent,
   },
   {
     path: 'achievement/:id',
