8bbb789ece35a284f070cbc07f1480b7a7d7e88f
sreejeet <vsreejeet@gmail.com>
2018-12-16 01:30:40 +0530

    UX upgrade.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   // Producation API
-  // baseUrl: 'https://amsmsi.com'
+  baseUrl: 'https://amsmsi.com'
 
   // Deploy test API
   // baseUrl: 'https://3.16.168.8:8080'
 
   // Development API
-  baseUrl: 'http://localhost:8080'
+  // baseUrl: 'http://localhost:8080'
 };
