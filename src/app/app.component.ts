6f01fe1be043029bb9d7456a8255b85dadaffd0e
sreejeet <vsreejeet@gmail.com>
2018-12-16 15:01:17 +0530

    Version info updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent implements OnInit {
   title = 'achievement-ms-front';
   date = new Date();
   constructor(private auth: AuthService, public router: Router) {
-    console.log('AMS Version 38');
+    console.log('AMS Version 39');
     setInterval(() => {
       this.date = new Date();
     }, 1000)
